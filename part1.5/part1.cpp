55a94f661c5564b10c2b26961c22da4bd9685461
Ian Johnson <enjalot@gmail.com>
2010-07-19 16:24:47 -0400

    made part1.5 tutorial which is part1 converted to use c++ bindings

---
+++ part1.5/part1.cpp
@@ -0,0 +1,100 @@
+#include <stdio.h>
+
+#include "cll.h"
+#include "util.h"
+
+void CL::popCorn()
+{
+    printf("in popCorn\n");
+
+    //initialize our kernel from the program
+    //kernel = clCreateKernel(program, "part1", &err);
+    //printf("clCreateKernel: %s\n", oclErrorString(err));
+    try{
+        kernel = cl::Kernel(program, "part1", &err);
+    }
+    catch (cl::Error er) {
+        printf("ERROR: %s(%d)\n", er.what(), er.err());
+    }
+
+       
+
+    //initialize our CPU memory arrays, send them to the device and set the kernel arguements
+    num = 10;
+    float *a = new float[num];
+    float *b = new float[num];
+    float *c = new float[num];
+    for(int i=0; i < num; i++)
+    {
+        a[i] = 1.0f * i;
+        b[i] = 1.0f * i;
+        c[i] = 0.0f;
+    }
+
+    printf("Creating OpenCL arrays\n");
+    size_t array_size = sizeof(float) * num;
+    //our input arrays
+    ///cl_a = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(float) * num, NULL, &err);
+    ///cl_b = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(float) * num, NULL, &err);
+    cl_a = cl::Buffer(context, CL_MEM_READ_ONLY, array_size, NULL, &err);
+    cl_b = cl::Buffer(context, CL_MEM_READ_ONLY, array_size, NULL, &err);
+    //our output array
+    ///cl_c = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(float) * num, NULL, &err);
+    cl_c = cl::Buffer(context, CL_MEM_READ_ONLY, array_size, NULL, &err);
+
+    printf("Pushing data to the GPU\n");
+    //push our CPU arrays to the GPU
+    ///err = clEnqueueWriteBuffer(command_queue, cl_a, CL_TRUE, 0, sizeof(float) * num, a, 0, NULL, &event);
+    err = queue.enqueueWriteBuffer(cl_a, CL_TRUE, 0, array_size, a, NULL, &event);
+    ///clReleaseEvent(event); //we need to release events in order to be completely clean (has to do with openclprof)
+    ///err = clEnqueueWriteBuffer(command_queue, cl_b, CL_TRUE, 0, sizeof(float) * num, b, 0, NULL, &event);
+    err = queue.enqueueWriteBuffer(cl_b, CL_TRUE, 0, array_size, b, NULL, &event);
+    ///clReleaseEvent(event);
+    ///err = clEnqueueWriteBuffer(command_queue, cl_c, CL_TRUE, 0, sizeof(float) * num, c, 0, NULL, &event);
+    err = queue.enqueueWriteBuffer(cl_c, CL_TRUE, 0, array_size, c, NULL, &event);
+    ///clReleaseEvent(event);
+    
+
+    //set the arguements of our kernel
+    ///err  = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *) &cl_a);
+    ///err  = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *) &cl_b);
+    ///err  = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *) &cl_c);
+    err = kernel.setArg(0, cl_a);
+    err = kernel.setArg(1, cl_b);
+    err = kernel.setArg(2, cl_c);
+    //Wait for the command queue to finish these commands before proceeding
+    ///clFinish(command_queue);
+    queue.finish();
+ 
+    //for now we make the workgroup size the same as the number of elements in our arrays
+    //workGroupSize[0] = num;
+}
+
+
+
+void CL::runKernel()
+{
+    printf("in runKernel\n");
+    //execute the kernel
+    ///err = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, workGroupSize, NULL, 0, NULL, &event);
+    err = queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(num), cl::NullRange, NULL, &event); 
+    ///clReleaseEvent(event);
+    printf("clEnqueueNDRangeKernel: %s\n", oclErrorString(err));
+    ///clFinish(command_queue);
+    queue.finish();
+
+    //lets check our calculations by reading from the device memory and printing out the results
+    float *c_done;
+    ///err = clEnqueueReadBuffer(command_queue, cl_c, CL_TRUE, 0, sizeof(float) * num, c_done, 0, NULL, &event);
+    err = queue.enqueueReadBuffer(cl_c, CL_TRUE, 0, sizeof(float) * num, c_done, NULL, &event);
+    printf("clEnqueueReadBuffer: %s\n", oclErrorString(err));
+    //clReleaseEvent(event);
+
+    for(int i=0; i < num; i++)
+    {
+        printf("c_done[%d] = %g\n", i, c_done[i]);
+    }
+
+}
+
+
---
+++ part1.5/util.cpp
@@ -0,0 +1,192 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <string.h>
+
+
+#include "cll.h"
+#include "util.h"
+
+char *file_contents(const char *filename, int *length)
+{
+    FILE *f = fopen(filename, "r");
+    void *buffer;
+
+    if (!f) {
+        fprintf(stderr, "Unable to open %s for reading\n", filename);
+        return NULL;
+    }
+
+    fseek(f, 0, SEEK_END);
+    *length = ftell(f);
+    fseek(f, 0, SEEK_SET);
+
+    buffer = malloc(*length+1);
+    *length = fread(buffer, 1, *length, f);
+    fclose(f);
+    ((char*)buffer)[*length] = '\0';
+
+    return (char*)buffer;
+}
+
+
+
+//NVIDIA's code follows
+//license issues probably prevent you from using this, but shouldn't take long
+//to reimplement
+//////////////////////////////////////////////////////////////////////////////
+//! Gets the platform ID for NVIDIA if available, otherwise default to platform 0
+//!
+//! @return the id 
+//! @param clSelectedPlatformID         OpenCL platform ID
+//////////////////////////////////////////////////////////////////////////////
+cl_int oclGetPlatformID(cl_platform_id* clSelectedPlatformID)
+{
+    char chBuffer[1024];
+    cl_uint num_platforms;
+    cl_platform_id* clPlatformIDs;
+    cl_int ciErrNum;
+    *clSelectedPlatformID = NULL;
+    cl_uint i = 0;
+
+    // Get OpenCL platform count
+    ciErrNum = clGetPlatformIDs (0, NULL, &num_platforms);
+    if (ciErrNum != CL_SUCCESS)
+    {
+        //shrLog(" Error %i in clGetPlatformIDs Call !!!\n\n", ciErrNum);
+        printf(" Error %i in clGetPlatformIDs Call !!!\n\n", ciErrNum);
+        return -1000;
+    }
+    else
+    {
+        if(num_platforms == 0)
+        {
+            //shrLog("No OpenCL platform found!\n\n");
+            printf("No OpenCL platform found!\n\n");
+            return -2000;
+        }
+        else
+        {
+            // if there's a platform or more, make space for ID's
+            if ((clPlatformIDs = (cl_platform_id*)malloc(num_platforms * sizeof(cl_platform_id))) == NULL)
+            {
+                //shrLog("Failed to allocate memory for cl_platform ID's!\n\n");
+                printf("Failed to allocate memory for cl_platform ID's!\n\n");
+                return -3000;
+            }
+
+            // get platform info for each platform and trap the NVIDIA platform if found
+            ciErrNum = clGetPlatformIDs (num_platforms, clPlatformIDs, NULL);
+            printf("Available platforms:\n");
+            for(i = 0; i < num_platforms; ++i)
+            {
+                ciErrNum = clGetPlatformInfo (clPlatformIDs[i], CL_PLATFORM_NAME, 1024, &chBuffer, NULL);
+                if(ciErrNum == CL_SUCCESS)
+                {
+                    printf("platform %d: %s\n", i, chBuffer);
+                    if(strstr(chBuffer, "NVIDIA") != NULL)
+                    {
+                        printf("selected platform %d\n", i);
+                        *clSelectedPlatformID = clPlatformIDs[i];
+                        break;
+                    }
+                }
+            }
+
+            // default to zeroeth platform if NVIDIA not found
+            if(*clSelectedPlatformID == NULL)
+            {
+                //shrLog("WARNING: NVIDIA OpenCL platform not found - defaulting to first platform!\n\n");
+                //printf("WARNING: NVIDIA OpenCL platform not found - defaulting to first platform!\n\n");
+                printf("selected platform: %d\n", 0);
+                *clSelectedPlatformID = clPlatformIDs[0];
+            }
+
+            free(clPlatformIDs);
+        }
+    }
+
+    return CL_SUCCESS;
+}
+
+
+// Helper function to get error string
+// *********************************************************************
+const char* oclErrorString(cl_int error)
+{
+    static const char* errorString[] = {
+        "CL_SUCCESS",
+        "CL_DEVICE_NOT_FOUND",
+        "CL_DEVICE_NOT_AVAILABLE",
+        "CL_COMPILER_NOT_AVAILABLE",
+        "CL_MEM_OBJECT_ALLOCATION_FAILURE",
+        "CL_OUT_OF_RESOURCES",
+        "CL_OUT_OF_HOST_MEMORY",
+        "CL_PROFILING_INFO_NOT_AVAILABLE",
+        "CL_MEM_COPY_OVERLAP",
+        "CL_IMAGE_FORMAT_MISMATCH",
+        "CL_IMAGE_FORMAT_NOT_SUPPORTED",
+        "CL_BUILD_PROGRAM_FAILURE",
+        "CL_MAP_FAILURE",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "CL_INVALID_VALUE",
+        "CL_INVALID_DEVICE_TYPE",
+        "CL_INVALID_PLATFORM",
+        "CL_INVALID_DEVICE",
+        "CL_INVALID_CONTEXT",
+        "CL_INVALID_QUEUE_PROPERTIES",
+        "CL_INVALID_COMMAND_QUEUE",
+        "CL_INVALID_HOST_PTR",
+        "CL_INVALID_MEM_OBJECT",
+        "CL_INVALID_IMAGE_FORMAT_DESCRIPTOR",
+        "CL_INVALID_IMAGE_SIZE",
+        "CL_INVALID_SAMPLER",
+        "CL_INVALID_BINARY",
+        "CL_INVALID_BUILD_OPTIONS",
+        "CL_INVALID_PROGRAM",
+        "CL_INVALID_PROGRAM_EXECUTABLE",
+        "CL_INVALID_KERNEL_NAME",
+        "CL_INVALID_KERNEL_DEFINITION",
+        "CL_INVALID_KERNEL",
+        "CL_INVALID_ARG_INDEX",
+        "CL_INVALID_ARG_VALUE",
+        "CL_INVALID_ARG_SIZE",
+        "CL_INVALID_KERNEL_ARGS",
+        "CL_INVALID_WORK_DIMENSION",
+        "CL_INVALID_WORK_GROUP_SIZE",
+        "CL_INVALID_WORK_ITEM_SIZE",
+        "CL_INVALID_GLOBAL_OFFSET",
+        "CL_INVALID_EVENT_WAIT_LIST",
+        "CL_INVALID_EVENT",
+        "CL_INVALID_OPERATION",
+        "CL_INVALID_GL_OBJECT",
+        "CL_INVALID_BUFFER_SIZE",
+        "CL_INVALID_MIP_LEVEL",
+        "CL_INVALID_GLOBAL_WORK_SIZE",
+    };
+
+    const int errorCount = sizeof(errorString) / sizeof(errorString[0]);
+
+    const int index = -error;
+
+    return (index >= 0 && index < errorCount) ? errorString[index] : "";
+
+}
+
